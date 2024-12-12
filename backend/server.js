90488d19434d4a1a569f1bf31d3d12fdcb1c6123
mandyh <mandyh@andrew.cmu.edu>
2024-12-12 16:42:45 -0500

    reverted code

--- backend/server.js
+++ backend/server.js
@@ -9,22 +9,22 @@ app.use(bodyParser.json());
 const axios = require('axios');
 const dataFilePath = 'data.json';
 
-const readDataFromFile = () => {
-    try {
-      const data = fs.readFileSync(dataFilePath, 'utf-8');
-      if (data && Array.isArray(data.data)) {
-        return data.data;
-      } else {
-        return [];
-      }
-    } catch (err) {
-      return []; // Return an empty array if file doesn't exist or is empty
-    }
-};
+// const readDataFromFile = () => {
+//     try {
+//       const data = fs.readFileSync(dataFilePath, 'utf-8');
+//       if (data && Array.isArray(data.data)) {
+//         return data.data;
+//       } else {
+//         return [];
+//       }
+//     } catch (err) {
+//       return []; // Return an empty array if file doesn't exist or is empty
+//     }
+// };
 
-const writeDataToFile = (data) => {
-    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
-};
+// const writeDataToFile = (data) => {
+//     fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2));
+// };
 
 async function fetchDataFromJetsonNano() {
     try {
@@ -40,14 +40,15 @@ app.get('/data', async (req, res) => {
     // Call this function to fetch data
     try {
         const data = await fetchDataFromJetsonNano(); // Fetch data from Jetson Nano
-        if (data && Array.isArray(data)) {
-            const old_data = readDataFromFile();
-            const updated_data = old_data.push(data);
-            writeDataToFile(updated_data);
-            res.status(200).send(updated_data);
-        } 
-        console.log('idk whats happening');
-        res.status(500).send([]);
+        // if (data && Array.isArray(data)) {
+        //     const old_data = readDataFromFile();
+        //     const updated_data = old_data.push(data);
+        //     writeDataToFile(updated_data);
+        //     res.status(200).send(updated_data);
+        // } 
+        // console.log('idk whats happening');
+        // res.status(500).send([]);
+        res.status(200).send(data);
     } catch (error) {
         res.status(500).send({ error: 'Error fetching data from Jetson Nano' });
     }
